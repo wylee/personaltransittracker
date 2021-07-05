159b43b6f24693b39cdd59b6c55072cdf4684ee1
Wyatt Baldwin <self@wyattbaldwin.com>
2021-07-05 04:34:47 -0700

    Replace GeoJSON stops view with MVT view
    MVT responses are smaller and load faster. The SQL query and view are
    also simpler than GeoJSON.

--- src/api/mystops/apps/stops/views.py
+++ src/api/mystops/apps/stops/views.py
@@ -1,15 +1,18 @@
+from django.conf import settings
 from django.db import connection
 from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.cache import cache_page
 
+from mystops.mvt import make_mvt_view
+
 from .models import Stop
 
 
-HALF_DAY_IN_SECONDS = 12 * 60 * 60
+CACHE_TIME = 30 if settings.DEBUG else (6 * 60 * 60)
 
 
-@cache_page(HALF_DAY_IN_SECONDS)
+@cache_page(CACHE_TIME)
 def index(request):
     """Return all stops as JSON."""
     stops = Stop.objects.all()
@@ -32,7 +35,7 @@ def index(request):
     )
 
 
-@cache_page(HALF_DAY_IN_SECONDS)
+@cache_page(CACHE_TIME)
 def stop(request, stop_id):
     stop = get_object_or_404(Stop, stop_id=stop_id)
     return JsonResponse(
@@ -47,83 +50,6 @@ def stop(request, stop_id):
     )
 
 
-@cache_page(HALF_DAY_IN_SECONDS)
-def geojson(request):
-    """Return a GeoJSON response."""
-    envelope = get_envelope(request)
-
-    if isinstance(envelope, HttpResponse):
-        return envelope
-
-    limit = request.GET.get("limit")
-    limit = int(limit) if limit else ""
-
-    statement = f"""
-        select row_to_json(feature_collection) as feature_collection from (
-          select
-            'FeatureCollection' as type,
-
-            (select items from (
-              select 'name' as type,
-              (select p from (select 'EPSG:4326' as name) as p) as properties
-            ) as items) as crs,
-
-            array_agg(features) as features from (
-              select
-                'Feature' as type,
-                ST_AsGeoJSON(stop.location)::json as geometry,
-                'stop.' || stop.stop_id::text as id,
-                (select p from (
-                  select
-                    stop.stop_id as id,
-                    stop.name,
-                    stop.direction,
-                    string_agg(route.short_name, ', ') as routes
-                  ) as p
-                ) as properties
-              from
-                stop
-              join
-                stop_route
-                on stop_route.stop_id = stop.id
-              join
-                route
-                on stop_route.route_id = route.id 
-              where
-                stop.location && {envelope}
-              group by
-                stop.id
-              {limit}
-            ) as features
-        ) as feature_collection
-    """
-
-    with connection.cursor() as cursor:
-        cursor.execute(statement)
-        row = cursor.fetchone()
-        if row is None:
-            return JsonResponse(None)
-        data = row[0]
-        if data["features"] is None:
-            data["features"] = []
-        return JsonResponse(row[0], safe=False)
-
-
-def get_envelope(request):
-    """Get envelope for bounding box query parameter ``bbox``."""
-    bbox = request.GET.get("bbox")
-    if not bbox:
-        return HttpResponseBadRequest("bbox query parameter is required")
-    coords = bbox.split(",")
-    if len(coords) != 4:
-        return HttpResponseBadRequest(f"Bad bounding box: {bbox}")
-    try:
-        coords = tuple(float(c) for c in coords)
-    except ValueError:
-        return HttpResponseBadRequest(f"Bad bounding box: {bbox}")
-    minx, miny, maxx, maxy = coords
-    if abs(minx) > 180 or abs(miny) > 90 or abs(maxx) > 180 or abs(maxy) > 90:
-        return HttpResponseBadRequest(f"Bad bounding box: {bbox}")
-    print(coords)
-    envelope = f"ST_MakeEnvelope({','.join(str(c) for c in coords)}, 4326)"
-    return envelope
+mvt = cache_page(CACHE_TIME)(
+    make_mvt_view("stop", "location", {"stop_id": "id", "name": None})
+)
